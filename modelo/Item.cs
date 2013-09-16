8ecdf8f8e397aa23a54112d665c8d68f6d625d8c
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-09-16 11:27:02 -0300

    Create Item.cs

---
+++ modelo/Item.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMonitor.modelo
+{
+    class Item
+    {
+        public Decimal id;
+        public String name;
+        public int bagindex;
+        public int bagslot;
+    }
+}
