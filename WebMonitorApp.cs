76ed4c6530892bd528535d079762992b88d6dd57
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-09-16 15:02:53 -0300

    Update WebMonitorApp.cs

--- WebMonitorApp.cs
+++ WebMonitorApp.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,11 +11,24 @@ namespace WebMonitor
 
         public readonly Guild guild;
         public readonly Character character;
-
+        private SendPlayer sPlayer = new SendPlayer(new Send());
+        
         public WebMonitorApp(Guild g, Character c)
         {
             guild = g;
             character = c;
         }
+
+        public void updateCharItens(List<Item> l)
+        {
+            character.listItensBag = l;
+            sPlayer.SentItensPlayer(character);
+        }
+
+        public void sendPlayerLogout()
+        {
+            sPlayer.SendPlayerLogout(character.id);
+        }
+
     }
 }
