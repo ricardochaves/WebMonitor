423d1b03004326f2aecc3616e45dbb1e2a2cf5a3
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-09-17 14:29:35 -0300

    Create GuildBankTab.cs

---
+++ modelo/GuildBankTab.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMonitor.modelo
+{
+    class GuildBankTab
+    {
+        public int Id;
+        public List<Item> listItensTab;
+
+    }
+}
