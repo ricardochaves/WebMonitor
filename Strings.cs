687f13094912d38f62aca498ec7efdd2e6977c47
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-10-02 11:33:58 -0300

    Update Strings.cs

--- Strings.cs
+++ Strings.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +15,7 @@ namespace WebMonitor
         public const string URLINCLUIRGUILDTOTAL = @"/api/saveguild";
         public const string URLSTARTNEWSESSION = @"/api/startsession";
         public const string URLCLOSESESSION = @"/api/closesession";
+        public const string URLCHECKSESSION = @"";
         public const string URLSENDPLAYERINFO = @"";
         public const string URLSENDPLAYERLOGOUT = @"";
         public const string URLSENDPLAYERITENS = @"";
