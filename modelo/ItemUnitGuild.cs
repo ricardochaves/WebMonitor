de22c7698d61631b3808b98c59b0b6f22b0d0e8e
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-11-22 13:48:22 -0200

    Create ItemUnitGuild.cs

---
+++ modelo/ItemUnitGuild.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMonitor.modelo
+{
+    public class ItemUnitGuild
+    {
+        Int64 idGuild;
+        Int64 idItem;
+        Int64 tabIndex;
+        Int64 tabSlot;
+        Int64 stackCount;
+    }
+}
