075c69f01146ad1cfe0ea3d1bd3acff895250449
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2013-11-22 13:46:58 -0200

    Update and rename ItemUnit.cs to ItemUnitChar.cs

---
+++ modelo/ItemUnitChar.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebMonitor.modelo
+{
+    public class ItemUnitChar
+    {
+        Int64 idChar;
+        Int64 idItem;
+        Int64 bagIndex;
+        Int64 bagSlot;
+        Int64 stackCount;
+    }
+}
